ae86b5dc2193d2f4b06879d58f0b8165eb7f0c2f
Ricardo Mejía <Ricardomejia218@gmail.com>
2023-10-23 12:21:22 -0600

    Guia9

---
+++ Guia 9/navegacion/src/navigation/ContactStack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/Home';
+const Stack= createStackNavigator();
+export default function ContactStack(){
+return(
+<Stack.Navigator>
+<Stack.Screen name="contact" component={Home}
+options={{title:'Contact'}}/>
+</Stack.Navigator>
+);
+}
